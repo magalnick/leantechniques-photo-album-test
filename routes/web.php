4b06bac9df6adecaf96771e9cb5f13eeb9cdedc7
David Magalnick <david@phone.com>
2022-12-26 02:10:02 -0800

    Build the UI and API controllers to have the photo album lookup as a web page

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('site.main-template', [
         'page'  => 'leantechniques-photo-album',
+        'js'    => 'leantechniques-photo-album',
         'title' => 'Photo Album',
     ]);
 });
